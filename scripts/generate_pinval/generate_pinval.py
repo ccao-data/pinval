15b2c38e7e441f3132f4b232622797e8b46ab142
Michael Wagner <wagnerleemichael@gmail.com>
2025-06-20 16:26:24 +0000

    Transform class and townships name

--- scripts/generate_pinval/generate_pinval.py
+++ scripts/generate_pinval/generate_pinval.py
@@ -229,12 +229,13 @@ def build_front_matter(
         front["cards"].append(
             {
                 "card_num": int(card_num),
+                "char_class_detailed": card_df["char_class_detailed"],
                 "location": {
                     k: v
                     for k, v in {
                         "property_address": card_df["property_address"],
                         "municipality": card_df.get("loc_tax_municipality_name"),
-                        "township": card_df["meta_township_code"],
+                        "township": card_df["township_name"],
                         "meta_nbhd_code": card_df["meta_nbhd_code"],
                         "loc_school_elementary_district_name": card_df.get("school_elementary_district_name"),
                         "loc_school_secondary_district_name": card_df.get("school_secondary_district_name"),
