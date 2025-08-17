42854ec5868ea783dc392ff574c75aff15c107bb
Jean Cochrane <jean@jeancochrane.com>
2025-08-17 11:35:26 -0500

    Fix bug preventing weights from being computed for some towns

--- scripts/generate_pinval/generate_pinval.py
+++ scripts/generate_pinval/generate_pinval.py
@@ -585,14 +585,10 @@ def compute_shap_weights(df: pd.DataFrame) -> pd.DataFrame:
     that is easier for readers to interpret than the raw weight.
     """
 
-    if not df.iloc[0]["is_report_eligible"]:
-        # Skip processing SHAP weights for PINs that are not eligible for
-        # reports. We only need to check the first row because all cards should
-        # have the same value for `is_report_eligible`
-        return df
-
     shap_cols = [col for col in df.columns if col.startswith("shap_")]
-    sum_df = df[shap_cols].abs().sum(axis=1)
+    # Sum the absolute values of every SHAP in each row, setting sums of zero
+    # to null to be safe (avoids division by zero)
+    sum_df = df[shap_cols].abs().sum(axis=1).replace(0, np.nan)
     weights_df = df[shap_cols].abs().div(sum_df, axis=0)
 
     # Cut the weights into predefined bins so that we can display them using a
