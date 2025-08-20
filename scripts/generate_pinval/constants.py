bdf4d52f063e57092e04e0433bba06b221bee727
Jean Cochrane <jean@jeancochrane.com>
2025-08-20 13:38:20 -0500

    Switch back to prod tables ahead of deploy

--- scripts/generate_pinval/constants.py
+++ scripts/generate_pinval/constants.py
@@ -2,6 +2,6 @@
 
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
-PINVAL_ASSESSMENT_CARD_TABLE = "z_dev_jecochr_pinval.vw_assessment_card"
-PINVAL_COMP_TABLE = "z_dev_jecochr_pinval.vw_comp"
-PINVAL_DATA_DICT_TABLE = "z_dev_jecochr_pinval.vars_dict"
+PINVAL_ASSESSMENT_CARD_TABLE = "pinval.vw_assessment_card"
+PINVAL_COMP_TABLE = "pinval.vw_comp"
+PINVAL_DATA_DICT_TABLE = "pinval.vars_dict"
