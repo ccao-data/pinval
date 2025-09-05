1a279b2ebd831c5174e533ee92e7a91253156bd3
Jean Cochrane <jean@jeancochrane.com>
2025-09-05 14:48:10 -0500

    Temporarily switch to dev comps table

--- scripts/generate_pinval/constants.py
+++ scripts/generate_pinval/constants.py
@@ -3,5 +3,5 @@
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
 PINVAL_ASSESSMENT_CARD_TABLE = "pinval.vw_assessment_card"
-PINVAL_COMP_TABLE = "pinval.vw_comp"
+PINVAL_COMP_TABLE = "z_dev_jecochr_pinval.vw_comp"
 PINVAL_DATA_DICT_TABLE = "pinval.vars_dict"
