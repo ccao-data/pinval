58fde1b8fe73b9c17189a61b7114fae2a61ba573
Michael Wagner <wagnerleemichael@gmail.com>
2025-09-09 17:59:26 +0000

    Add dev table

--- scripts/generate_pinval/constants.py
+++ scripts/generate_pinval/constants.py
@@ -4,4 +4,6 @@
 # need to switch to dev tables for testing
 PINVAL_ASSESSMENT_CARD_TABLE = "pinval.vw_assessment_card"
 PINVAL_COMP_TABLE = "pinval.vw_comp"
-PINVAL_DATA_DICT_TABLE = "pinval.vars_dict"
+PINVAL_DATA_DICT_TABLE = (
+    "z_ci_edit_pinval_seed_for_num_sales_prev_n_years_description_pinval.vars_dict"
+)
