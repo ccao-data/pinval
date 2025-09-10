501d32530d28fe252ff2e36e2f5e64a56b8bcfe5
Michael Wagner <wagnerleemichael@gmail.com>
2025-09-10 18:39:52 +0000

    Revert table to prod

--- scripts/generate_pinval/constants.py
+++ scripts/generate_pinval/constants.py
@@ -2,8 +2,6 @@
 
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
-PINVAL_ASSESSMENT_CARD_TABLE = (
-    "z_ci_add_proration_data_to_pinval_assessment_pin_pinval.vw_assessment_card"
-)
+PINVAL_ASSESSMENT_CARD_TABLE = "pinval.vw_assessment_card"
 PINVAL_COMP_TABLE = "pinval.vw_comp"
 PINVAL_DATA_DICT_TABLE = "pinval.vars_dict"
