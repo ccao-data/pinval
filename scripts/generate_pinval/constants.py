4a3c47c65da1171c00e1f55165f03d7cbea6522a
Jean Cochrane <jean@jeancochrane.com>
2025-09-11 10:52:16 -0500

    Deploy alternative significant sales to staging

--- scripts/generate_pinval/constants.py
+++ scripts/generate_pinval/constants.py
@@ -3,5 +3,9 @@
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
 PINVAL_ASSESSMENT_CARD_TABLE = "pinval.vw_assessment_card"
-PINVAL_COMP_TABLE = "pinval.vw_comp"
-PINVAL_DATA_DICT_TABLE = "pinval.vars_dict"
+PINVAL_COMP_TABLE = (
+    "z_ci_jeancochrane_deploy_alt_comps_algorithm_to_staging_pinval.vw_comp"
+)
+PINVAL_DATA_DICT_TABLE = (
+    "z_ci_jeancochrane_deploy_alt_comps_algorithm_to_staging_pinval.vars_dict"
+)
